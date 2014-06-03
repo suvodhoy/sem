5cbdca5182b004f3b62697816032b4cb05502440
Salil Panikkaveettil <salilpa007@gmail.com>
2014-06-03 21:09:36 +0530

    Adding django bootstrap to the mixup

--- sem/settings.py
+++ sem/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration'
+    'registration',
+    'bootstrap_toolkit',
 )
 
 MIDDLEWARE_CLASSES = (
